99763617b0d4103f0e501fd029559a8e5057184b
Wadwadw <57014179+Wadwadw@users.noreply.github.com>
2021-10-28 11:53:17 +0300

    add lead model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('YouTube', 'YouTube'),
+        ('Google', 'Google'),
+        ('NewsLetter', 'NewsLetter'),
+    )
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    special_files = models.FileField(blank=True, null=True)
+
