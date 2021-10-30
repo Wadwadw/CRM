9bdb9d72261a960b9f920060ad6ace478ee0b17a
Wadwadw <57014179+Wadwadw@users.noreply.github.com>
2021-10-30 18:27:40 +0300

    html

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Lead
 
-# Create your views here.
+def home_page(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads
+    }
+    return render(request, 'second_page.html', context)
