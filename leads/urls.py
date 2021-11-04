bb3d433190a1ff2a0278cc61b9ce50fc8bd85100
vadym <sumsunh777@gmail.com>
2021-11-04 12:28:34 +0200

    add views and css

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import lead_list, lead_detail, lead_create
+from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', lead_list),
-    path('<int:pk>/', lead_detail),
-    path('create/', lead_create)
+    path('', lead_list, name='list-lead'),
+    path('<int:pk>/', lead_detail, name='detail-lead'),
+    path('create/', lead_create, name='create-lead'),
+    path('<int:pk>/update/', lead_update, name='update-lead'),
+    path('<int:pk>/delete/', lead_delete, name='delete-lead')
 ]
