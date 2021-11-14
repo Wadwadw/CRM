8d725ba565b04c82682b71a11c95911ed9e857c8
Wadwadw <57014179+Wadwadw@users.noreply.github.com>
2021-11-14 12:56:56 +0200

    html

--- CRM/urls.py
+++ CRM/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
-    path('signup/', SignupView.as_view(), name='signup' ),
+    path('agents/', include('agents.urls', namespace='agents')),
+    path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
